80f5e252f584ecc9f86932796033c7a2a34c230f
becem gharbi <becem.gharbi@live.com>
2023-07-23 14:30:11 +0100

    feat(auth): Add login & logout methods

--- src/runtime/composables/useDirectusAuth.ts
+++ src/runtime/composables/useDirectusAuth.ts
@@ -2,8 +2,11 @@ import { authentication } from "@directus/sdk";
 import { useRuntimeConfig } from "#app";
 import useDirectus from "./useDirectus";
 import type { Authentication } from "../types/config";
+import type { LoginCredentials } from "../types/index";
 
 export default function useDirectusAuth() {
+  const loggedIn = useState("directus-logged-in", () => false);
+
   const config = useRuntimeConfig().public.directus.auth as Authentication;
 
   const client = useDirectus().with(
@@ -14,5 +17,26 @@ export default function useDirectusAuth() {
     })
   );
 
-  return client;
+  async function login(credentials: LoginCredentials) {
+    const route = useRoute();
+
+    return client
+      .login(credentials.email, credentials.password)
+      .then(async () => {
+        const returnToPath = route.query.redirect?.toString();
+        const redirectTo = returnToPath || config.redirect.home;
+        loggedIn.value = true;
+        return navigateTo(redirectTo);
+      });
+  }
+
+  async function logout() {
+    return client.logout().then(async () => {
+      clearNuxtData();
+      loggedIn.value = false;
+      return navigateTo(config.redirect.logout);
+    });
+  }
+
+  return { login, logout };
 }
