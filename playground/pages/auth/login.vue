ee71f1eb9ef464b80ee34afa800f1c7f8f411aa1
Dominic <domi.m.98@googlemail.com>
2023-11-11 16:52:57 +0100

    FEAT: update playground login to insert mail + pass

--- playground/pages/auth/login.vue
+++ playground/pages/auth/login.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
     <h1>Login</h1>
-    <button @click="login('tester@test.com', 'test123')">
-      Login
-    </button>
-    <button @click="loginWithProvider('google')">
-      Login with google
-    </button>
-    <button @click="requestPasswordReset('becem.gharbi@live.com')">
-      Request password reset
-    </button>
+    <div>
+      <label>E-Mail:</label>
+      <input v-model="email" type="email" placeholder="Email">
+    </div>
+    <div>
+      <label>Password:</label>
+      <input v-model="password" type="password" placeholder="Password">
+    </div>
+
+    <div>
+      <button @click="login(email, password)">
+        Login
+      </button>
+      <button @click="loginWithProvider('google')">
+        Login with google
+      </button>
+      <button @click="requestPasswordReset('becem.gharbi@live.com')">
+        Request password reset
+      </button>
+    </div>
   </div>
 </template>
 
@@ -18,4 +29,19 @@
 definePageMeta({ middleware: 'guest' })
 
 const { login, loginWithProvider, requestPasswordReset } = useDirectusAuth()
+
+const email = ref('tester@test.com')
+const password = ref('test123')
 </script>
+
+<style scoped>
+div {
+  margin-top: 1rem;
+}
+
+button + button,
+input {
+  margin-left: 0.5rem;
+}
+
+</style>
