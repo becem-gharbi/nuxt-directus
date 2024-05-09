431d18937c4971cb05f63a3f3758f6a9fd4bba42
becem-gharbi <becem.gharbi@live.com>
2024-05-09 16:09:34 +0100

    types(useDirectusRest): set unset `DirectusSchema` type to `any` #84

--- src/runtime/composables/useDirectusRest.ts
+++ src/runtime/composables/useDirectusRest.ts
@@ -2,7 +2,7 @@ import type { RestCommand } from '@directus/sdk'
 import { useNuxtApp } from '#imports'
 
 export async function useDirectusRest<Output extends object> (
-  options: RestCommand<Output, DirectusSchema>
+  options: RestCommand<Output, keyof DirectusSchema extends never ? any : DirectusSchema>
 ): Promise<Output> {
   return await useNuxtApp().$directus.client.request<Output>(options)
 }
