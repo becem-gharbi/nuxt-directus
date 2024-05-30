40e5138ed8db020f2d747e1119202c9c3b6c424d
becem-gharbi <becem.gharbi@live.com>
2024-05-30 08:37:11 +0100

    test: install playwright

---
+++ tests/basic.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test'
+import { goto, login, reload } from './utils'
+
+test('should be logged in', async ({ browser }) => {
+})
+
+test('should refresh session', async ({ browser }) => {
+})
