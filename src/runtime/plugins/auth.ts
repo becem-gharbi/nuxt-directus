d4404804334ba0763fdd1831d1b9ba6c2d0aadb4
becem-gharbi <becem.gharbi@live.com>
2024-06-17 17:52:09 +0100

    refactor: assign names to used plugins

--- src/runtime/plugins/auth.ts
+++ src/runtime/plugins/auth.ts
@@ -7,61 +7,66 @@ import {
   useRouter,
 } from '#imports'
 
-export default defineNuxtPlugin(async (nuxtApp) => {
-  const config = nuxtApp.$config.public.directus as PublicConfig & { auth: { enabled: true } }
-  const { _loggedInFlag, _refreshOn, refresh, autoRefresh } = useDirectusSession()
-  const { user, _onLogout, fetchUser } = useDirectusAuth()
-  const { currentRoute } = useRouter()
+export default defineNuxtPlugin({
+  name: 'directus:auth',
+  dependsOn: ['directus:rest'],
 
-  _refreshOn.value = 0
+  setup: async (nuxtApp) => {
+    const config = nuxtApp.$config.public.directus as PublicConfig & { auth: { enabled: true } }
+    const { _loggedInFlag, _refreshOn, refresh, autoRefresh } = useDirectusSession()
+    const { user, _onLogout, fetchUser } = useDirectusAuth()
+    const { currentRoute } = useRouter()
 
-  nuxtApp.hook('directus:loggedIn', (state) => {
-    _loggedInFlag.value = state ? 1 : 0
-  })
+    _refreshOn.value = 0
 
-  nuxtApp.hook('app:mounted', () => {
-    addEventListener('storage', (event) => {
-      if (event.key === config.auth.loggedInFlagName) {
-        if (event.oldValue === '1' && event.newValue === '0' && user.value) {
-          _onLogout()
-        }
-        else if (event.oldValue === '0' && event.newValue === '1') {
-          location.reload()
-        }
-      }
+    nuxtApp.hook('directus:loggedIn', (state) => {
+      _loggedInFlag.value = state ? 1 : 0
     })
-  })
 
-  function isFirstTime() {
-    const isPageFound = currentRoute.value?.matched.length > 0
-    const isPrerenderd = typeof nuxtApp.payload.prerenderedAt === 'number'
-    const isServerRendered = nuxtApp.payload.serverRendered
-    return (import.meta.server && !isPrerenderd && isPageFound) || (import.meta.client && (!isServerRendered || isPrerenderd || !isPageFound))
-  }
+    nuxtApp.hook('app:mounted', () => {
+      addEventListener('storage', (event) => {
+        if (event.key === config.auth.loggedInFlagName) {
+          if (event.oldValue === '1' && event.newValue === '0' && user.value) {
+            _onLogout()
+          }
+          else if (event.oldValue === '0' && event.newValue === '1') {
+            location.reload()
+          }
+        }
+      })
+    })
 
-  async function isExpired() {
-    const authData = await useDirectusStorage().get()
-    const now = new Date().getTime()
-    return !authData?.expires_at || authData.expires_at < now + config.auth.msRefreshBeforeExpires!
-  }
+    function isFirstTime() {
+      const isPageFound = currentRoute.value?.matched.length > 0
+      const isPrerenderd = typeof nuxtApp.payload.prerenderedAt === 'number'
+      const isServerRendered = nuxtApp.payload.serverRendered
+      return (import.meta.server && !isPrerenderd && isPageFound) || (import.meta.client && (!isServerRendered || isPrerenderd || !isPageFound))
+    }
 
-  function canFetchUser() {
-    const isSSO = currentRoute.value?.path === config.auth.redirect.callback && !currentRoute.value.query.reason
-    const { _loggedInFlag, _refreshToken, _sessionToken } = useDirectusSession()
-    return isSSO || _loggedInFlag.value || _refreshToken.get() || _sessionToken.get()
-  }
+    async function isExpired() {
+      const authData = await useDirectusStorage().get()
+      const now = new Date().getTime()
+      return !authData?.expires_at || authData.expires_at < now + config.auth.msRefreshBeforeExpires!
+    }
 
-  if (isFirstTime() && canFetchUser()) {
-    await isExpired()
-      ? await refresh().then(b => b ? fetchUser() : null)
-      : await fetchUser()
-  }
+    function canFetchUser() {
+      const isSSO = currentRoute.value?.path === config.auth.redirect.callback && !currentRoute.value.query.reason
+      const { _loggedInFlag, _refreshToken, _sessionToken } = useDirectusSession()
+      return isSSO || _loggedInFlag.value || _refreshToken.get() || _sessionToken.get()
+    }
 
-  if (user.value) {
-    await autoRefresh(true)
-    await nuxtApp.callHook('directus:loggedIn', true)
-  }
-  else {
-    _loggedInFlag.value = 0
-  }
+    if (isFirstTime() && canFetchUser()) {
+      await isExpired()
+        ? await refresh().then(b => b ? fetchUser() : null)
+        : await fetchUser()
+    }
+
+    if (user.value) {
+      await autoRefresh(true)
+      await nuxtApp.callHook('directus:loggedIn', true)
+    }
+    else {
+      _loggedInFlag.value = 0
+    }
+  },
 })
--- src/runtime/plugins/rest.ts
+++ src/runtime/plugins/rest.ts
@@ -4,54 +4,58 @@ import type { PublicConfig } from '../types'
 import { useDirectusStorage } from '../composables/useDirectusStorage'
 import { defineNuxtPlugin, useRequestHeaders, useRequestEvent } from '#imports'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const config = nuxtApp.$config.public.directus as PublicConfig & { auth: { enabled: true } }
-  const event = useRequestEvent()
-  const reqHeaders = useRequestHeaders(['cookie'])
+export default defineNuxtPlugin({
+  name: 'directus:rest',
 
-  const fetch = $fetch.create({
-    onRequest({ options }) {
-      if (import.meta.server) {
-        options.headers = {
-          ...options.headers,
-          ...reqHeaders,
+  setup: (nuxtApp) => {
+    const config = nuxtApp.$config.public.directus as PublicConfig & { auth: { enabled: true } }
+    const event = useRequestEvent()
+    const reqHeaders = useRequestHeaders(['cookie'])
+
+    const fetch = $fetch.create({
+      onRequest({ options }) {
+        if (import.meta.server) {
+          options.headers = {
+            ...options.headers,
+            ...reqHeaders,
+          }
         }
-      }
-    },
-    onResponse({ response }) {
-      if (import.meta.server) {
-        const cookies = splitCookiesString(response.headers.get('set-cookie') ?? '')
-
-        for (const cookie of cookies) {
-          appendResponseHeader(event!, 'set-cookie', cookie)
-        }
-      }
-    },
-  })
-
-  const directus = createDirectus<DirectusSchema>(config.rest.baseUrl, {
-    globals: {
-      fetch,
-    },
-  })
-
-  const client = directus
-    .with(rest({
-      credentials: config.auth.mode === 'session' ? 'include' : 'same-origin',
-    }))
-    .with(authentication(config.auth.mode, {
-      autoRefresh: false,
-      msRefreshBeforeExpires: config.auth.msRefreshBeforeExpires,
-      credentials: 'include',
-      storage: useDirectusStorage(),
-    }))
-
-  return {
-    provide: {
-      directus: {
-        client,
-        _refreshTimeout: null,
       },
-    },
-  }
+      onResponse({ response }) {
+        if (import.meta.server) {
+          const cookies = splitCookiesString(response.headers.get('set-cookie') ?? '')
+
+          for (const cookie of cookies) {
+            appendResponseHeader(event!, 'set-cookie', cookie)
+          }
+        }
+      },
+    })
+
+    const directus = createDirectus<DirectusSchema>(config.rest.baseUrl, {
+      globals: {
+        fetch,
+      },
+    })
+
+    const client = directus
+      .with(rest({
+        credentials: config.auth.mode === 'session' ? 'include' : 'same-origin',
+      }))
+      .with(authentication(config.auth.mode, {
+        autoRefresh: false,
+        msRefreshBeforeExpires: config.auth.msRefreshBeforeExpires,
+        credentials: 'include',
+        storage: useDirectusStorage(),
+      }))
+
+    return {
+      provide: {
+        directus: {
+          client,
+          _refreshTimeout: null,
+        },
+      },
+    }
+  },
 })
